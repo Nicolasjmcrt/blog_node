ab86689f3de649caa4160d063ad40cb556aa4808
Nicolasjmcrt <jumeaucourtn@gmail.com>
2022-09-26 19:35:46 +0200

    create different path

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/post', (req, res) => {
+    res.send('Got a GET request at /post')
+  })
+
+
+app.post('/post', function (req, res) {
+    res.send('Got a POST request at /post');
+  });
+
+app.put('/post', function (req, res) {
+    res.send('Got a PUT request at /post');
+});
+
+app.delete('/post', function (req, res) {
+    res.send('Got a DELETE request at /post');
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
